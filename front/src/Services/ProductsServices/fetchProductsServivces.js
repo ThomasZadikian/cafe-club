20fc9ea871371ab5042a4fc09565f968026fd250
Thomas <thomas.zadikian@businessdecision.com>
2024-06-04 12:32:31 +0200

    add services files

---
+++ front/src/Services/ProductsServices/fetchProductsServivces.js
@@ -0,0 +1,5 @@
+/* TODO : 
+- Ajout de la logique de vérification des informations
+- Ajout de l'appel au controller pour récupérer les données
+- Ajout de la logique de vérification des données reçues
+*/
