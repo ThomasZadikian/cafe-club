46036d17751987ff3a4857df40e6c3812e72a421
Thomas <thomas.zadikian@businessdecision.com>
2024-07-23 16:09:37 +0200

    Refacto for usersInsert

--- front/src/Services/UserServices/insertUserServices.js
+++ front/src/Services/UserServices/insertUserServices.js
@@ -4,41 +4,38 @@ import fetchUser from "./fetchUserServices";
 const usernameVerification = async (formData) => {
   const username = formData.get("username");
   const email = formData.get("email");
-  let users = [];
-  if (username === null || email === null) {
+
+  if (!username || !email) return false;
+
+  try {
+    const users = await fetchUser(formData);
+    return !(users && users.length > 0);
+  } catch (error) {
     return false;
-  } else {
-    try {
-      users = await fetchUser(formData);
-    } catch (error) {
-      return false;
-    }
-    return !!(users?.length === 0 || users === undefined);
   }
 };
 
 export const insertUserVerification = async (formData) => {
-  const username = formData.get("username");
-  const email = formData.get("email");
-  const password = formData.get("password");
-  if (await usernameVerification(formData)) {
-    try {
-      const response = await fetch(`${BASE_API_URL}users/insert`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          username: username,
-          email: email,
-          password: password,
-        }),
-      });
-      return !!response.ok;
-    } catch (error) {
-      return false;
-    }
-  } else {
+  const [username, email, password] = ["username", "email", "password"].map(
+    (key) => formData.get(key)
+  );
+
+  if (!(await usernameVerification(formData))) return false;
+
+  try {
+    const response = await fetch(`${BASE_API_URL}users/insert`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        email: email,
+        password: password,
+      }),
+    });
+    return !!response.ok;
+  } catch (error) {
     return false;
   }
 };
