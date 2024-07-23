8d9a9759a5599e385d2973c7f8f13659acadbbb0
Thomas <thomas.zadikian@businessdecision.com>
2024-07-23 16:01:57 +0200

    Add pattern for username Form

--- front/src/Components/userForm.js
+++ front/src/Components/userForm.js
@@ -39,12 +39,12 @@ const UserForm = () => {
       {errorAppear ? (
         <ErrorMessage message={errorMessage} error={errorAppear} />
       ) : null}
-      <h2 className="text-2xl font-semibold mb-6">
-        Formulaire d'ajout d'utilisateur
-      </h2>
+      <h2 className="text-2xl font-semibold mb-6">Créez votre compte</h2>
       <form onSubmit={handleSubmit} className="space-y-4 ">
         <div>
-          <label className="block font-medium mb-2">Nom de l'utilisateur</label>
+          <label className="block font-medium mb-2">
+            Votre nom / pseudonyme
+          </label>
           <input
             className="w-full border-2 border-gold px-3 py-2 rounded text-gray-900"
             type="text"
@@ -55,19 +55,25 @@ const UserForm = () => {
           />
         </div>
         <div>
-          <label className="block font-medium mb-2">Adresse e-mail:</label>
+          <label className="block font-medium mb-2">Votre adresse email</label>
           <input
+            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+            type="email"
             className="w-full border-2 border-gold px-3 py-2 rounded text-gray-900"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            required
           />
         </div>
         <div>
-          <label className="block font-medium mb-2">Mot de passe</label>
+          <label className="block font-medium mb-2">
+            Votre mot de passe (doit contenir au moins 8 caractère)
+          </label>
           <input
             className="w-full border-2 border-gold px-3 py-2 rounded text-gray-900"
-            type="text"
+            type="password"
             value={password}
+            pattern="(?=.*\d)(?=.*[a-z]).{8,}"
             onChange={(e) => setPassword(e.target.value)}
           />
         </div>
