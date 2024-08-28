b50f10069dab8be5aa0940bc4fd4c4c410e5a988
Thomas <thomas.zadikian@businessdecision.com>
2024-08-28 11:09:36 +0200

    fix issues with SQL error

--- front/src/back/db/db.js
+++ front/src/back/db/db.js
@@ -1,11 +1,12 @@
 const mysql = require("mysql2");
-require("dotenv").config({ path: "../../.env" });
+require("dotenv").config();
 
 const connect = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 module.exports = connect;
